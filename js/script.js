12635248682a84b1cf74cdce7b3ab483c5458da1
AntonioDattolo <antonio19794.a@gmail.com>
2024-05-13 18:08:32 +0200

    BONUS 2

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const slides = [
     }
 ];
 
-console.log(slides);
+// console.log(slides);
 
 const { createApp } = Vue
 
@@ -30,46 +30,44 @@ const { createApp } = Vue
     data() {
       return {
         message: 'Hello Vue!',
-        slides : [
-                {
-                    image: './img/01.webp',
-                    title: 'Marvel\'s Spiderman Miles Morale',
-                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                }, {
-                    image: './img/02.webp',
-                    title: 'Ratchet & Clank: Rift Apart',
-                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                }, {
-                    image: './img/03.webp',
-                    title: 'Fortnite',
-                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                }, {
-                    image: './img/04.webp',
-                    title: 'Stray',
-                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                }, {
-                    image: './img/05.webp',
-                    title: "Marvel's Avengers",
-                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                }
-            ],
-        imgSel : 0, 
+          slides: [
+              {
+                  image: './img/01.webp',
+                  title: 'Marvel\'s Spiderman Miles Morale',
+                  text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+              }, {
+                  image: './img/02.webp',
+                  title: 'Ratchet & Clank: Rift Apart',
+                  text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+              }, {
+                  image: './img/03.webp',
+                  title: 'Fortnite',
+                  text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+              }, {
+                  image: './img/04.webp',
+                  title: 'Stray',
+                  text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+              }, {
+                  image: './img/05.webp',
+                  title: "Marvel's Avengers",
+                  text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+              }
+        ],
+        imgSel : 0,
+        autoplay : null
       }
     }, 
     methods: {
         selected(index){
           if(index == this.imgSel){
-            return "active"
+            return "active" 
           }else{
             return ""
-          }  
-        },
+          } 
+        }, 
         next(){
-            if(this.imgSel < slides.length - 1){
-            this.imgSel++
-            } else{
-                this.imgSel = 0
-            }
+           this.play()
+            clearInterval(autoplay)
         },
         prev(){
             if(this.imgSel > 0 ){
@@ -80,6 +78,16 @@ const { createApp } = Vue
         },
         chose(index){
             this.imgSel = index
+        },
+        play(){ if(this.imgSel < slides.length - 1){
+            this.imgSel++
+            } else{
+                this.imgSel = 0
+            }
         }
+        
+    },
+    mounted(){    
+        autoplay = setInterval(this.play,3000)
     }
   }).mount('#app')
